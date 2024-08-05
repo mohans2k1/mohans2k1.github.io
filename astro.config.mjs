c91f01730506c73b66fff26f02bea93a09c090ed
Mohan <mothish07122001@gmail.com>
2024-08-05 23:56:23 +0530

    SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,33 +4,28 @@ import netlify from "@astrojs/netlify";
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
-
 import solidJs from "@astrojs/solid-js";
 import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
-
 import svelte from "@astrojs/svelte";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://msundaram.dev/",
-  integrations: [
-    sitemap(),
-    robotsTxt({
-      sitemap: [
-        "https://gianmarco.xyz/sitemap-index.xml",
-        "https://gianmarco.xyz/sitemap-0.xml",
-      ],
-    }),
-    solidJs(),
-    UnoCSS({ injectReset: true }),
-    icon(),
-    svelte(),
-  ],
+  integrations: [sitemap(), robotsTxt({
+    sitemap: ["https://gianmarco.xyz/sitemap-index.xml", "https://gianmarco.xyz/sitemap-0.xml"]
+  }), solidJs(), UnoCSS({
+    injectReset: true
+  }), icon(), svelte()],
   markdown: {
-    remarkPlugins: [remarkReadingTime],
+    remarkPlugins: [remarkReadingTime]
   },
   output: "server",
   vite: {
-    assetsInclude: "**/*.riv",
+    assetsInclude: "**/*.riv"
   },
+  adapter: node({
+    mode: "standalone"
+  })
 });
