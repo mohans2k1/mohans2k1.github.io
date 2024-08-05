af55658465b8765f66d35da1ba722bb83b1783e4
Mohan <mothish07122001@gmail.com>
2024-08-05 23:30:29 +0530

    initial commit

---
+++ src/lib/constants.ts
@@ -0,0 +1,11 @@
+export const LINKS = {
+  github: "https://github.com/mohans2k1",
+  linkedin: "https://www.linkedin.com/in/mohana-sundaram-s/",
+  books:   "https://plain-root-f29.notion.site/5da66373e88d4512bcf293bf9792aeec?v=64479220c0e44b21aae3459181fab819"
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
